f3405f0f5606aa04b143eafb2978293a06c81c7c
Jace Simons <jacesimons10@gmail.com>
2026-07-05 00:06:28 -0600

    trigger and icon

---
+++ src/components/Icons/StopIconFilled.tsx
@@ -0,0 +1,11 @@
+import { BaseIcon, IconProps } from "./BaseIcon";
+
+export const StopIconFilled = (props: IconProps) => {
+  return (
+    <BaseIcon {...props}>
+      <path d="M216,56V200a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V56A16,16,0,0,1,56,40H200A16,16,0,0,1,216,56Z"></path>
+    </BaseIcon>
+  );
+};
+
+export default StopIconFilled;
