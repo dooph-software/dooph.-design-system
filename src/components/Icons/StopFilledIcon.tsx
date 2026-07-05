2a4c119b368bb8f482226d1f8e716d0fccb0aed2
Jace Simons <jacesimons10@gmail.com>
2026-07-05 17:07:19 -0600

    stop filled icon

--- src/components/Icons/StopFilledIcon.tsx
+++ src/components/Icons/StopFilledIcon.tsx
@@ -1,6 +1,6 @@
 import { BaseIcon, IconProps } from "./BaseIcon";
 
-export const StopIconFilled = (props: IconProps) => {
+export const StopFilledIcon = (props: IconProps) => {
   return (
     <BaseIcon {...props}>
       <path d="M216,56V200a16,16,0,0,1-16,16H56a16,16,0,0,1-16-16V56A16,16,0,0,1,56,40H200A16,16,0,0,1,216,56Z"></path>
@@ -8,4 +8,4 @@ export const StopIconFilled = (props: IconProps) => {
   );
 };
 
-export default StopIconFilled;
+export default StopFilledIcon;
