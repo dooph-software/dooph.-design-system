7c942bba6fb2a7695947b1ed7954c61fcfdaf62b
Jace Simons <jacesimons10@gmail.com>
2026-07-05 00:10:07 -0600

    trigger and icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -57,6 +57,7 @@ export { ShowMoneyIcon } from "./ShowMoneyIcon";
 export { SiloIcon } from "./SiloIcon";
 export { SortAscendingIcon } from "./SortAscendingIcon";
 export { SortDescendingIcon } from "./SortDescendingIcon";
+export { StopIcon } from "./StopIcon";
 export { SystemThemeIcon } from "./SystemThemeIcon";
 export { TableIcon } from "./TableIcon";
 export { ThinkingIcon } from "./ThinkingIcon";
